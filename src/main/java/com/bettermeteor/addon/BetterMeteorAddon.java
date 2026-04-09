d806f8198c3af5169f93e9e4a8a5b5ba9f76cc9b
Holly Yi <hollyyiishere@gmail.com>
2026-04-09 19:28:32 +0800

    initial commit

--- src/main/java/com/bettermeteor/addon/BetterMeteorAddon.java
+++ src/main/java/com/bettermeteor/addon/BetterMeteorAddon.java
@@ -1,24 +1,20 @@
 package com.bettermeteor.addon;
 
-import com.bettermeteor.addon.commands.CommandExample;
 import com.bettermeteor.addon.hud.EnderPearlInfoHud;
+import com.bettermeteor.addon.modules.HandChams;
 import com.bettermeteor.addon.modules.SignCommand;
-import com.bettermeteor.addon.hud.HudExample;
 import com.bettermeteor.addon.modules.HumanAutoTotem;
 import com.bettermeteor.addon.modules.ItemNametags;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
 public class BetterMeteorAddon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("BetterMeteor");
     public static final HudGroup HUD_GROUP = new HudGroup("BetterMeteor");
 
     @Override
@@ -29,18 +25,10 @@ public class BetterMeteorAddon extends MeteorAddon {
         Modules.get().add(new SignCommand());
         Modules.get().add(new HumanAutoTotem());
         Modules.get().add(new ItemNametags());
-
-        // Commands
-        Commands.add(new CommandExample());
+        Modules.get().add(new HandChams());
 
         // HUD
-        Hud.get().register(HudExample.INFO);
         Hud.get().register(EnderPearlInfoHud.INFO);
-    }
-
-    @Override
-    public void onRegisterCategories() {
-        Modules.registerCategory(CATEGORY);
     }
 
     @Override
